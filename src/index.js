a6b37e2eb2e11186fa9e403e922f938b4edae43f
rostislav_kastov <valynoka@yandex.ru>
2022-03-31 20:22:17 +0300

    deploy на githab

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {HashRouter} from "react-router-dom";
-// import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename="/index.html">
           <App />
-      </HashRouter>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
